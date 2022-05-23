fac66a575011a686ff75044b05cd460bd87aad7b
Zachary Sain <zachsain@gmail.com>
2022-05-23 17:47:32 -0500

    completed

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -22,10 +22,32 @@ class BirdsController < ApplicationController
     end
   end
 
+  def update 
+    bird = Bird.find_by(id: params[:id])
+    
+    if bird 
+      updated = bird.update(bird_params)
+      render json: updated
+    else 
+      render json: {error: "bird not found"}, status: :not_found 
+    end 
+
+  end 
+
+  def increment_likes
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.update(likes: bird.likes + 1)
+      render json: bird
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
-    params.permit(:name, :species)
+    params.permit(:name, :species, :likes)
   end
 
 end
